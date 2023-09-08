002e15306ae80bedff39a7ab916262d4cbcaed58
TheShadowC <carlosmmart@hotmail.com>
2023-09-08 12:48:02 -0300

    Conexion
    Creación de conexión!

--- src/AccesoADatos/Conexion.java
+++ src/AccesoADatos/Conexion.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AccesoADatos;
 
-/**
- *
- * @author USUARIO
- */
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class Conexion {
-    
+
+    private static final String URL = "jdbc:mysql://localhost/";
+    private static final String DB = "universidadulp";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "p21Mu61354";
+
+    private static Connection connection;
+
+    private Conexion() {
+    }
+
+    public static Connection getConexion() {
+
+        if (connection == null) {
+            try {
+                //Class.forName("org.mariadb.jdbc.Driver"); //MariaDB
+                Class.forName("com.mysql.cj.jdbc.Driver"); //MySQLDB
+                connection = DriverManager
+                        .getConnection(URL + DB + "?useLegacyDatetimeCode=false&serverTimezone=UTC" + "&user=" + USUARIO + "&password=" + PASSWORD);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error al conectarse a la Base de Datos "+ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null,"Error al cargar los Drivers de la Base de Datos "+ex.getMessage());
+            }
+        }
+        return connection;
+    }
 }
--- src/proyectotransversal_grupo36/ProyectoTransversal_grupo36.java
+++ src/proyectotransversal_grupo36/ProyectoTransversal_grupo36.java
@@ -1,12 +1,14 @@
 
 package proyectotransversal_grupo36;
 
+import AccesoADatos.Conexion;
+
 
 public class ProyectoTransversal_grupo36 {
 
     
     public static void main(String[] args) {
-   
+        Conexion.getConexion();
     }
     
 }//borre algunos comentarios y agregue este
