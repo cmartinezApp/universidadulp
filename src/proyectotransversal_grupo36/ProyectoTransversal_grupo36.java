abebba16b07b58527d88e9e690f7dfdcad03433d
Susana Goro <su_goro@hotmail.com>
2023-09-08 08:54:32 -0300

    Susana realizo el main de nombre proyecto transversal_grupo36 sigo intentando

--- src/proyectotransversal_grupo36/ProyectoTransversal_grupo36.java
+++ src/proyectotransversal_grupo36/ProyectoTransversal_grupo36.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectotransversal_grupo36;
 
-/**
- *
- * @author Usuario
- */
+
 public class ProyectoTransversal_grupo36 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+   
     }
     
-}//
+}//borre algunos comentarios y agregue este
