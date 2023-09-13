d58b4bdb98bc62eae1d5c09b19f17ef474de0163
TheShadowC <carlosmmart@hotmail.com>
2023-09-13 11:46:09 -0300

    conector sql
    conector sql

--- src/proyectotransversal_grupo36/ProyectoTransversal_grupo36.java
+++ src/proyectotransversal_grupo36/ProyectoTransversal_grupo36.java
@@ -9,6 +9,7 @@ public class ProyectoTransversal_grupo36 {
     
     public static void main(String[] args) {
         Conexion.getConexion();
+        System.out.println("Conexión establecida");
         Conexion.cerrarConexion();
     }
     
